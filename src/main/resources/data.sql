adec6f0570f44d97c33524bdc63f42e065968018
GirijalaAditya <girijalaaditya@gmail.com>
2021-11-18 19:04:51 +0530

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into TODO
+values(100, 'Learn Linux', false, sysdate(), 'aditya');
+insert into TODO
+values(102, 'Learn AWS', false, sysdate(), 'aditya');
+insert into TODO
+values(103, 'Learn DEVOPS', false, sysdate(), 'aditya');
